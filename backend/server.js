9535f666dec852d592a8ad7a63b99b2b632164f0
aditi-sonar22 <adiofv22@gmail.com>
2026-03-21 15:02:41 +0530

    Connected mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://aditi:Adi2205@ac-4adycx9-shard-00-00.smvhc8h.mongodb.net:27017,ac-4adycx9-shard-00-01.smvhc8h.mongodb.net:27017,ac-4adycx9-shard-00-02.smvhc8h.mongodb.net:27017/?ssl=true&replicaSet=atlas-ysqi6e-shard-0&authSource=admin&appName=Cluster0")
+.then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+});
+
 const express = require('express');
 const app = express();
 
